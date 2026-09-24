5a6b4a8e3656eb1646ba7e3a6f4fdcfa00f72efd
ayushbhatt0 <ayushbhatt7417@gmail.com>
2026-09-24 12:21:03 +0530

    completing exception handling

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def get_requirements()->List[str]:
             lines = file.readlines()
             for line in lines:
                 requirement= line.strip()
-                if requirement and requirement != "-e.":
+                if requirement and requirement != "-e .":
                     requirement_lst.append(requirement)
     except FileNotFoundError:
         print("requirements.txt file not found")
@@ -29,8 +29,8 @@ setup(
     name = "NetworkSecurity",
     version = "0.0.1",
     author = "Ayush Bhatt",
-    author_email = "ayushbhatt.ml@gmail.com"
-    packages = find_packages()
+    author_email = "ayushbhatt.ml@gmail.com",
+    packages = find_packages(),
     install_requires = get_requirements()
 
 )
